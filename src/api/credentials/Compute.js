ce31998d9a14e3aea5b1269313b267057f2bf726
SniperFox213 <dj.gleb@list.ru>
2020-12-26 11:01:43 +0200

    Tried to fix GCP Authentication

--- src/api/credentials/Compute.js
+++ src/api/credentials/Compute.js
@@ -2,12 +2,12 @@
 // let's create new @google-cloud/compute instance
 import Compute from "@google-cloud/compute";
 
-import fs from "fs";
-import gcc from "./gcc.json";
+// import fs from "fs";
+// import gcc from "./gcc.json";
 
 // And now let's create file, containing authentication
 // information.
-fs.writeFile('./gcc-authentication.json', JSON.stringify(gcc), (error) => {});
+// fs.writeFile('./gcc-authentication.json', JSON.stringify(gcc), (error) => {});
 
 // And now let's
 const compute = new Compute({ projectId: "unfull-cloud", keyFilename: "./gcc-authentication.json" });
